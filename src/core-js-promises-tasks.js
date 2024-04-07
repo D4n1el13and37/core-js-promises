2e52089652e1966be5cac69aeae0dac39aa46e98
D4n1el13and37 <daniel837@mail.ru>
2024-04-07 14:37:36 +0300

    feat: solve getFirstResolvedPromiseResult task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -58,8 +58,16 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return Promise.all(
+    promises.map((promise) => promise.catch(() => undefined))
+  ).then((result) => {
+    const syu = result.find((succ) => succ !== undefined);
+    if (syu) {
+      return syu;
+    }
+    throw Error('');
+  });
 }
 
 /**
